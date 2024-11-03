6beb2fcfaa9c829975ebc7a168399f566dd38217
marcocapote <marcocapote15@gmail.com>
2024-11-02 21:57:51 -0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
-
+app.use(express.json());
 app.use(express.static('../Ludify'));
 const port = 3000;
-app.get('/hello', (req, res) => {
-    res.send("Hello, world");
+
+const usuarios = [];
+
+const router = express.Router();
+app.get('/api/usuarios', (req, res) => {
+    res.status(200).json(usuarios);
+});
+
+app.post('api/usuarios', (req, res) => {
+    var usuario = req.body;
+    usuario.id = 1;
+    usuarios.push(usario);
+    res.status(201).json(usuario);
 });
 
 app.listen(port, () => {
