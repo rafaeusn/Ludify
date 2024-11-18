bb7a278fd12f4485b885fdc5cc21041f22ff4af8
rafaeusn <rafael.c.sene@Hotmail.com>
2024-11-18 02:46:44 -0300

    Delete routes directory

--- routes/jogoRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/jogosRoutes.js
-const express = require('express');
-const router = express.Router();
-const jogosController = require('../controllers/jogoController');
-
-// Defina as rotas específicas para jogos, por exemplo:
-router.post('/jogos', jogosController.createJogo);
-
-module.exports = router;
--- routes/usuarioRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/usuarioRoutes.js
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-router.post('/usuarios', usuarioController.createUsuario);
-
-module.exports = router;
